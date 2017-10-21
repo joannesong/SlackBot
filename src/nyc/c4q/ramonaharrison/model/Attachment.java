be0cc701c3980aff2b1539da598c31650e806f34
Amy Rivera <amyrivera.ar@gmail.com>
2017-10-21 18:28:09 -0400

    fix again

--- src/nyc/c4q/ramonaharrison/model/Attachment.java
+++ src/nyc/c4q/ramonaharrison/model/Attachment.java
@@ -3,8 +3,10 @@ package nyc.c4q.ramonaharrison.model;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.util.ArrayList;
 
 
+public class Attachments {
 
     // TODO: implement private fields for each of the following attachment JSON keys:
     private String fallback;
@@ -77,3 +79,46 @@ import org.json.simple.JSONObject;
         }
 
     }
+    public String getfallback(){
+        return fallback;
+    }
+    public String getcolor(){
+        return color;
+    }
+    public String getpretext(){
+        return pretext;
+    }
+    public String getauthod_name(){
+        return author_name;
+    }
+    public String getauthor_link(){
+        return author_link;
+    }
+    public String getauthor_icon(){
+        return author_icon;
+    }
+    public String gettitle(){
+        return title;
+    }
+    public String gettext(){
+        return text;
+    }
+    public ArrayList<Field> getfields(){
+        return fields;
+    }
+    public String getimage_url(){
+        return image_url;
+    }
+    public String getthumb_url(){
+        return thumb_url;
+    }
+    public String getfooter(){
+        return footer;
+    }
+    public String getfooter_icon(){
+        return footer_icon;
+    }
+    public Long getts(){
+        return ts;
+    }
+}
