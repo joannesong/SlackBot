65810d50509370a8199902a661d5352d7241318e
Kevin Toro <kevintoro@ac.c4q.nyc>
2017-10-21 18:05:24 -0400

    Kevin's Branch commit

--- src/nyc/c4q/ramonaharrison/Main.java
+++ src/nyc/c4q/ramonaharrison/Main.java
@@ -14,11 +14,6 @@ public class Main {
 
         myBot.listMessages(Slack.BOTS_CHANNEL_ID);
 
-        // Post "Hello, world!" to the #bots channel
-        //myBot.sendMessage("Hello, world!");
-
-        // Post a pineapple photo to the #bots channel
-        //myBot.sendMessage("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
 
     }
 }
